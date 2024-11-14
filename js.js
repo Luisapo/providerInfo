c4d2a39efca11ae832820df799e19cbca8908b7a
Luisapo <luis.javier.apodaca@gmail.com>
2024-11-13 20:52:29 -0700

    Add feature for PCP Change and Strucure

--- js.js
+++ js.js
@@ -1,3 +1,50 @@
+const PCPChange = (providername, npi, address) => {
+    const message = `From: Clinica La Familia
+
+In order to have everything ready for your appointment you need to contact your insurance company and update your information, please call the member services phone number located on the back of your insurance card and:
+
+Update your primary care provider (PCP) EFFECTIVE TODAY by giving them the following info:
+
+- Name of the physician: ${providername}
+- NPI: ${npi}
+- Tax ID: 861039168
+- Address: ${address}
+- Office Phone: 602-569-3999
+
+PLEASE DONT FORGET TO ASK FOR YOUR REFERENCE NUMBER AND WRITE IT DOWN. If you are told that you don't have a reference number, please ask for the representative's name and the initial of their last name, and make a note of the date of the call. THANK YOU`;
+
+    // Copy to clipboard
+    navigator.clipboard.writeText(message).then(() => {
+        alert("Message copied to clipboard");
+    }).catch(err => {
+        console.error("Could not copy text: ", err);
+    });
+};
+
+const PCPChangeEspanol = (providername, npi, address) => {
+    const message = `
+    De: Clínica La Familia
+
+Para tener todo listo para su cita, es necesario que llame a su compañía de seguro y actualice sus datos. Por favor, llame al numero de servicio al cliente que aparece al reverso de su tarjeta de seguro y:
+
+Actualice su medico primario (PCP) A PARTIR DE HOY  proporcionándoles la siguiente información:
+
+- Nombre: ${providername}
+- NPI: ${npi}
+- Tax ID: 861039168
+- Dirección: ${address}
+- Numero telefónico: 602-569-3999
+
+POR FAVOR NO OLVIDE PREGUNTAR POR UN NUMERO DE REFERENCIA Y ESCRIBIRLO **si le dicen que no tiene numero de referencia pida el nombre y la inicial del apellido del representante y anote la fecha de la llamada** GRACIAS
+`
+navigator.clipboard.write(message).then(() =>{
+    alert("Message copied to clipboard");
+}).catch(err => {
+    console.error("Could not copy text: ", err);
+})
+
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     fetch("./providerData.json")
         .then(response => response.json())
@@ -10,44 +57,54 @@ document.addEventListener('DOMContentLoaded', () => {
             thead.classList.add("tableHeader");
             const headerRow = document.createElement('tr');
             
-            Object.keys(data[0]).forEach((header, index) => {
+            Object.keys(data[0]).forEach(header => {
                 const headerCell = document.createElement('th');
-                headerCell.textContent = header;
-                
-                
-            
-                // if (index === 0) {
-                //     // Add a button to the first header
-                //     const filterButton = document.createElement('button');
-                //     filterButton.textContent = 'Name';
-                //     filterButton.addEventListener('click', () => {
-                //         // Do this later, add the handle button click for the first header
-
-                //     });
-                //     headerCell.appendChild(filterButton);
-                // }
-            
+                headerCell.textContent = header;                
                 headerRow.appendChild(headerCell);
             });
             
             thead.appendChild(headerRow);
             providerTableData.appendChild(thead);
-            
 
             // Create table body
             const tbody = document.createElement('tbody');
 
             data.forEach(rowData => {
                 const row = document.createElement('tr');
-
-                Object.values(rowData).forEach(value => {
+                
+                Object.entries(rowData).forEach(([key, value]) => {
                     const cell = document.createElement('td');
                     cell.textContent = value || '';
-                    row.appendChild(cell);
+                    row.appendChild(cell); 
+                });
+                
+                const PCPChangeCell = document.createElement('td');
+                PCPChangeCell.className= 'pcpChanger';
+                const newButtonEnglish = document.createElement('button');
+                newButtonEnglish.textContent = "PCP Change";
+                newButtonEnglish.style.backgroundColor = "Azure";
+                
+                const newButtonSpanish = document.createElement('button');
+                newButtonSpanish.textContent = "Cambio de PCP";
+                newButtonSpanish.style.backgroundColor = "Coral";
+                
+                // Add click event for English button
+                newButtonEnglish.addEventListener('click', () => {
+                    PCPChange(rowData.Name, rowData.NPI, rowData.Location);
                 });
 
+                newButtonSpanish.addEventListener('click', () => {
+                    PCPChangeEspanol(rowData.Name, rowData.NPI, rowData.Location);
+                })
+
+                PCPChangeCell.appendChild(newButtonEnglish);                
+                PCPChangeCell.appendChild(newButtonSpanish)
+                row.appendChild(PCPChangeCell);
                 tbody.appendChild(row);
             });
+
+            providerTableData.appendChild(tbody);
+            document.body.appendChild(providerTableData);
 
             providerTableData.appendChild(tbody);
 
@@ -69,27 +126,27 @@ document.addEventListener('DOMContentLoaded', () => {
                 let hasTempe = false;
 
                 cells.forEach(cell => {
-                    if (cell.textContent === 'CLF West/Glendale - 7734 N 59th Ave Glendale AZ 85301') {
+                    if (cell.textContent === '7734 N 59th Ave Glendale AZ 85301') {
                         hasWest = true;
                         return; 
-                    } else if(cell.textContent === 'CLF Laveen - 4250 W Baseline Rd Phoenix, AZ 85041'){
+                    } else if(cell.textContent === '4250 W Baseline Rd Phoenix, AZ 85041'){
                     hasLaveen = true;
                         return;
-                    }else if(cell.textContent === 'CLF Mesa - 616 E Southern Ave #103,Mesa AZ 85204') {
+                    }else if(cell.textContent === '616 E Southern Ave #103,Mesa AZ 85204') {
                         hasMesa = true;
                         return;
-                    } else if(cell.textContent === 'CLF El Mirage - 15235 N Dysart Rd El Mirage AZ 85335'){
+                    } else if(cell.textContent === '15235 N Dysart Rd Ste 103, El Mirage, AZ 85335'){
                         hasElMirage = true;
                         return;
-                    }else if (cell.textContent === 'CLF North - 13402 N 32nd St Phoenix AZ 85032'){
+                    }else if (cell.textContent === '13402 N 32nd St Phoenix AZ 85032'){
                         hasNorth = true;
-                    }else if (cell.textContent === 'CLF Downtown - 1533 E Willetta St Phoenix AZ 85006'){
+                    }else if (cell.textContent === '1533 E Willetta St Phoenix AZ 85006'){
                         hasCentro = true;
                         return;
-                    } else if(cell.textContent === 'CLF Casa Grande - 301 E Cottonwood Ln Casa Grande AZ 85122'){
+                    } else if(cell.textContent === '301 E Cottonwood Ln Casa Grande AZ 85122'){
                         hasCasaGrande = true;
                         return;
-                    } else if(cell.textContent === 'CLF Tempe - 1315 W Southern Ave Tempe, AZ 85282'){
+                    } else if(cell.textContent === '1315 W Southern Ave Tempe, AZ 85282'){
                         hasTempe = true;
                         return;
                     }else if(cell.textContent === "Clinica La Familia"){
@@ -99,7 +156,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 });
 
                 if (hasWest) {
-                    row.style.backgroundColor = '#D2E0FB'; // Set the row's background color to green
+                    row.style.backgroundColor = '#D2E0FB'; 
                 }if(hasLaveen){
                     row.style.backgroundColor = '#F9F3CC';
                 }if(hasMesa){
@@ -124,56 +181,3 @@ document.addEventListener('DOMContentLoaded', () => {
             
         });
 });
-
-const providers = [    
-    'Clinica La Familia',
-    'Aaron J Jensen', 
-    'Alex Guzman Garcia', 
-    'Alma Navarro',
-    'Amanda Rascon',
-    'Andrew C. White',
-    'Blair Ball',
-    'Carlomagno Calderon Briones',
-    'Carmen M. Rivera',
-    'Celina Ruiz Escamilla',
-    'Christine C Briones',
-    'Claudia Ivone Romo',
-    'Drisde Isabel Cruz Martinez',
-    'Elizabeth Lopez-Murray',
-    'Erick G. Gonzalez',
-    'Erick Torres',
-    'Fernando Galaviz',
-    'Flor N Arellano',
-    'Francis Rahul Luciano',
-    'Freddy L Montenegro',
-    'Gloria A. Estrada',
-    'Grace Stracuzzi',
-    'Haidee Chavez Waymire',
-    'J. Guadalupe Gallo Padilla',
-    'Javier G Padilla',
-    'Jennyfer Willford',
-    'Jessica Cuevas',
-    'Julia Calderon',
-    'Julia Nieto',
-    'Maria Del Carmen Castillo',
-    'Nina Alexandria Celaya',
-    'Ricardo G. Celaya',
-    'Ruby E Fernandez',
-    'Russell Patrick Jackson',
-    'Sam F. Shumway',
-    'Sandy Pauline Morales',
-    'Seth Evert Gillespie',
-    'Walter Rios-Corujo',
-    'Xochitl Landeros',
-    'Yesenia E. Ochoa']
-
-    const clinics = [
-        'North',
-        'West',
-        'Downtown',
-        'Mesa',
-        'Tempe',
-        'Casa Grande',
-        'El Mirage',
-        'Laveen'
-    ]
